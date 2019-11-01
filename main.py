f670d40fdb138ee89ff87cb8220f095f2af8536b
João Vitor Roma <jvmr2.0@gmail.com>
2019-10-31 22:02:11 -0300

    Merge conflict

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import cv2
 from Hough import HoughTransform
 
 img = cv2.imread('olho-teste.png')
-output=img.copy()
+#output=img.copy()
+output=cv2.imread('olho.jpg')
 hough = HoughTransform(img, 148, 152, 0, 1, 60, 120, 240, 300)
 
 for (y, x, r, acc) in hough.coord_center:
